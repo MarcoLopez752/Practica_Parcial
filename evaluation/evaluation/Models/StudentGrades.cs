a33010e2c4556995bc82630521abb4e5d5e1fcc2
Marco López guevara <marco752slayer@gmail.com>
2019-09-27 10:33:55 -0400

    Second commit

---
+++ evaluation/evaluation/Models/StudentGrades.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace evaluation.Models
+{
+    public enum Subjects
+    {
+        biology,
+        math,
+        physics,
+        chemistry,
+        history,
+        languague,
+        physical_education
+
+    }
+
+
+
+
+    public class StudentGrades
+    {
+        [Key]
+        public int DetailID { get; set; }
+        public int StuID { get; set; }
+        public Subjects subjects { get; set; }
+        public int score { get; set; }
+        public StudentMain StudentMain { get; set; }
+
+
+
+    }
+}
---
+++ evaluation/evaluation/Models/StudentMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace evaluation.Models
+{
+
+    public enum Genre
+    {
+        male,
+        female
+    }
+
+    public class StudentMain
+    {
+        [Key]
+        public int StuID { get; set; }
+        public string StudentName { get; set; }
+        public Genre genre { get; set; }
+        public string Date_of_birth { get; set; }
+        public virtual ICollection<StudentGrades> StudentGrades { get; set; }
+
+
+
+
+    }
+}
